c334b235b4fc3c1b82fafd5999adcfa16da8a58a
Matthias Langer <mlangc@gmx.at>
2015-09-20 08:33:29 +0200

    Prepare for comparing new with old `SourceUtils` implementations

--- src/test/scala/com/github/mlangc/experiments/refactoring/SourceUtilsBenchmark.scala
+++ src/test/scala/com/github/mlangc/experiments/refactoring/SourceUtilsBenchmark.scala
@@ -21,17 +21,44 @@ object SourceUtilsBenchmark extends Quickbenchmark {
 
   private val input = Gen.single("testSource")(Input(testSource))
 
-  performance of "SourceUtils" in {
-    measure method "stripComment" config(exec.benchRuns -> 500) in {
-      using(input) in { input =>
-        SourceUtils.stripComment(input.source)
+  private trait SourceUtilsAdapter {
+    def countRelevantBrackets(source: String, open: Char, close: Char): (Int, Int)
+    def stripComment(source: String): String
+    def name: String
+  }
+
+  private implicit class LegacySourceUtilsAdapter(underlying: LegacySourceUtils) extends SourceUtilsAdapter {
+    override def countRelevantBrackets(source: String, open: Char, close: Char) = underlying.countRelevantBrackets(source, open, close)
+    override def stripComment(source: String): String = underlying.stripComment(source)
+    override def name = "LegacySourceUtils"
+  }
+
+  private implicit class ActualSourceUtilsAdapter(underlying: SourceUtils) extends SourceUtilsAdapter {
+    override def countRelevantBrackets(source: String, open: Char, close: Char) = underlying.countRelevantBrackets(source, open, close)
+    override def stripComment(source: String): String = underlying.stripComment(source)
+    override def name = "SourceUtils"
+  }
+
+  private def runTests(adapter: SourceUtilsAdapter, testStripComment: Boolean = false, testCountBrackets: Boolean = false): Unit = {
+    performance of adapter.name in {
+      if (testStripComment) {
+        measure method "stripComment" config(exec.benchRuns -> 500) in {
+          using(input) in { input =>
+            adapter.stripComment(input.source)
+          }
+        }
       }
 
-      measure method "countRelevantBrackets" in {
-        using(input) config(exec.benchRuns -> 500) in { input =>
-          SourceUtils.countRelevantBrackets(input.source, '(', ')')
+      if (testCountBrackets) {
+        measure method "countRelevantBrackets" in {
+          using(input) config(exec.benchRuns -> 500) in { input =>
+            adapter.countRelevantBrackets(input.source, '(', ')')
+          }
         }
       }
     }
   }
+
+  runTests(LegacySourceUtils, testCountBrackets = true)
+  runTests(SourceUtils, testCountBrackets = true)
 }
